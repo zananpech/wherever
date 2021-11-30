6a0ffa618a840dfb9596b7b66ddb9e9b30290e36
Mohammed Abufarhana <realmohaa@mailbox.unideb.hu>
2021-12-01 00:44:00 +0100

    Added the Contact page, Removed boilerplate code, minor updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Feature from './pages/Feature.jsx';
 import Third from './pages/Third.jsx'
-import React, {useEffect} from 'react';
+import Contact from './pages/Contact.jsx'
+import React, { useEffect } from 'react';
 import ReactGa from 'react-ga';
-import {Animated} from "react-animated-css";
+import { Animated } from "react-animated-css";
 
 
 function App() {
@@ -19,34 +20,35 @@ function App() {
     ReactGa.initialize('UA-179333339-1');
     ReactGa.pageview('/');
   })
-  return ( 
+  return (
     <div>
       <div class="lg:flex m-5 h-screen">
-        <div class="lg:w-1/2">
-          <img class="mx-auto lg:w-4/5 lg:h-4/5" src={require("./assets/progess.svg")}/>
+        <div class="lg:w-1/2 lg:h-3/5 mt-48">
+          <img class="mx-auto md:w-4/5 md:h-4/5" src={require("./assets/progess.svg")} />
         </div>
-          <div class="flex flex-col justify-center items-center lg:items-start">
-            <Animated animationIn="slideInUp">
-                <h1 class="font-semibold lg:text-5xl text-3xl lg:text-left text-center mb-3">
-                  Track progress <br />
-                  instead of Time
-                </h1>
-            </Animated>
-            <Animated animationInDelay="800">
-              <p class="md:text-2xl text-lg text-center">
-                Track what get's done in an easy understand <br/>
-                <span>virtual way and share your work in real time</span> 
-              </p>
-            </Animated>
-            <button onClick={clickHandler} class="font-bold text-lg mt-10 bg-purple-500 hover:bg-purple-700 rounded-lg p-3 text-white w-40 items-center">
-                    I'm interested
-            </button>
-          </div>
+        <div class="flex flex-col justify-center items-center lg:items-start">
+          <Animated animationIn="slideInUp">
+            <h1 class="font-semibold lg:text-5xl text-3xl lg:text-left text-center mb-3">
+              Track progress <br />
+              instead of Time
+            </h1>
+          </Animated>
+          <Animated animationInDelay="800">
+            <p class="md:text-2xl text-lg text-center">
+              Track what get's done in an easy understand <br />
+              <span>virtual way and share your work in real time</span>
+            </p>
+          </Animated>
+          <button onClick={clickHandler} class="font-bold text-lg mt-10 transition-all bg-purple-500 hover:bg-purple-800 rounded-lg p-3 text-white w-40 items-center">
+            I'm interested
+          </button>
+        </div>
       </div>
       {/* second */}
-        <Feature/>
-        <Third/>
-    </div> 
+      <Feature />
+      <Third />
+      <Contact />
+    </div>
   );
 }
 
